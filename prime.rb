e7d77124473fccd2c0a7c2422c108eef753ecfc7
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-29 23:25:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
 def prime?(num)
+  if num < 0
+    false
+  else
+    
   (2..Math.sqrt(num)).each do |i|
-    if num % i === 0 || num < 0
+    if num % i === 0
       false
     end
   end
   true
+end
 end
