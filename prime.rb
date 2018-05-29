9a72d0fdf67a4d1b07003a7c6605f2a790146caf
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-29 23:37:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ require 'bigdecimal/math'
 
 def prime?(num)
   return false if num <= 1
-  (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
-    true
+  (2..Math.sqrt(num)).each { |i| return false if num % i == 0 }
+  true
 end
 
 puts Benchmark.measure { BigMath.PI(10_000) }
